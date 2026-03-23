7249a1a5b7d8fe4f1073447ac8d4e956fe44987b
Ali Ahmad <whozahm3d@gmail.com>
2026-03-23 07:08:16 +0500

    Apply useful app UI guardrails

---
+++ movie_recommendation/__init__.py
@@ -0,0 +1,40 @@
+from .analysis import (
+    dataset_summary,
+    project_scorecard,
+    recommendation_preview,
+    recommendation_quality_summary,
+    revenue_summary,
+    top_genres,
+)
+from .data import load_movie_data
+from .recommender import (
+    OFFICIAL_RECOMMENDATION_PIPELINE,
+    RecommenderArtifacts,
+    RecommendationPipelineSpec,
+    build_official_recommender,
+    build_recommender,
+    describe_official_pipeline,
+    personalized_recommendations,
+    recommend_by_title,
+)
+from .revenue import RevenueModelArtifacts, train_revenue_model
+
+__all__ = [
+    "OFFICIAL_RECOMMENDATION_PIPELINE",
+    "RecommendationPipelineSpec",
+    "RecommenderArtifacts",
+    "RevenueModelArtifacts",
+    "build_official_recommender",
+    "build_recommender",
+    "dataset_summary",
+    "describe_official_pipeline",
+    "load_movie_data",
+    "personalized_recommendations",
+    "project_scorecard",
+    "recommend_by_title",
+    "recommendation_preview",
+    "recommendation_quality_summary",
+    "revenue_summary",
+    "top_genres",
+    "train_revenue_model",
+]
